c8c2d4f5ff83b2ad172de62e74ee4009062d2228
Yantao Liu <3285695776@qq.com>
2026-06-11 13:31:55 +0800

    refactor: 从 Element Plus 切换至 Ant Design Vue

--- mall-web/src/main.js
+++ mall-web/src/main.js
@@ -1,20 +1,12 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 import router from './router'
 import pinia from './store'
 
 const app = createApp(App)
-
-// 注册所有 Element Plus 图标
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
-app.use(ElementPlus, { locale: zhCn })
+app.use(Antd)
 app.use(router)
 app.use(pinia)
 app.mount('#app')
