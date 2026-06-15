f801a094e7091171d29c17773c907dbbebfd8b87
Yantao Liu <3285695776@qq.com>
2026-06-15 13:50:10 +0800

    fix: 添加 @MapperScan 解决 MyBatis Mapper 无法注入的问题

--- src/main/java/com/mall/web/MallApplication.java
+++ src/main/java/com/mall/web/MallApplication.java
@@ -1,11 +1,13 @@
 package com.mall.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.mall")
 @EnableScheduling
+@MapperScan("com.mall.**.mapper")
 public class MallApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
